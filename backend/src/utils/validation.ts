87b62680ea5c3ae33ce86b2f91a81cad8889102c
subhamnayak76 <subhamnayak9638@gmail.com>
2025-05-05 23:11:52 +0530

    login

--- backend/src/utils/validation.ts
+++ backend/src/utils/validation.ts
@@ -1,3 +1,4 @@
+import { Language } from "@prisma/client"
 import {z} from "zod"
 export const registerSchema = z.object({
     email : z.string().email({message: "wrong email "}),
@@ -11,4 +12,10 @@ export const loginSchema = z.object({
     password : z.string().min(6,{message:"min 6 chars required in for password"})
 })
     
+export const submitSchema = z.object({
+    code : z.string().min(1,{message : "provide code "}),
+    language : z.string().min(1,{message : "provide the code language"}),
+    problemid: z.string().min(1,{message: "add the problemid"}),
+    userId : z.string().min(1,{message : 'provide the user id'})
 
+})
